e50d9b56d2bce6712b5f54b9a02498a78cbe3304
mtraub <mario.traub@ergon.ch>
2024-07-01 11:34:47 +0200

    Responsive

--- Projekt/Responsive/js/slider.js
+++ Projekt/Responsive/js/slider.js
@@ -1,23 +1,17 @@
-import {getRandomGame} from "./getGame.js";
-
-const slidesContainer = document.getElementById("slides-container") as HTMLDivElement;
-
-fillInSlider()
-
-const slide = document.querySelector(".recommendation") as HTMLElement;
-const prevButton = document.getElementById("slide-arrow-prev") as HTMLButtonElement;
-const nextButton = document.getElementById("slide-arrow-next") as HTMLButtonElement;
-
+import { getRandomGame } from "./getGame.js";
+const slidesContainer = document.getElementById("slides-container");
+fillInSlider();
+const slide = document.querySelector(".recommendation");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
 nextButton.addEventListener("click", (event) => {
     const slideWidth = slide.clientWidth;
     slidesContainer.scrollLeft += slideWidth;
 });
-
 prevButton.addEventListener("click", () => {
     const slideWidth = slide.clientWidth;
     slidesContainer.scrollLeft -= slideWidth;
 });
-
 function fillInSlider() {
     for (let i = 0; i < 5; i++) {
         getRandomGame().then((game) => {
